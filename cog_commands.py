f54021dfc6daa77a37cea7c437dfa9f8885e72f0
Prashant9683 <gehlotprashant968@gmail.com>
2022-01-26 14:57:52 +0530

    final commit 2

--- cog_commands.py
+++ cog_commands.py
@@ -8,10 +8,11 @@ class cog_commands(commands.Cog):
         self.bot = bot
         self.is_playing = False
         self.music_queue = []
+        self.hidden_music_queue = []
         self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
         self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
-        self.vc = ""
+        self.voice = ""
+        self.greeting_message = "Hey there! I am your new music assistant!!"
         self.help_message = """
 !help - Shows this message
 !play <song_name> - Plays the song
@@ -33,109 +34,108 @@ class cog_commands(commands.Cog):
     def play_next(self):
         if len(self.music_queue) > 0:
             self.is_playing = True
-            m_url = self.music_queue[0][0]['source']
+            video_source = self.music_queue[0][0]['source']
             self.music_queue.pop(0)
-
-            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+            self.voice.play(discord.FFmpegPCMAudio(video_source, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
         else:
             self.is_playing = False
 
     def play_previous(self):
         if len(self.music_queue) >= 0:
-            m_url = self.music_queue[0][0]['source']
-            self.music_queue.pop(0)
-            self.vc.music_queue.position -= 2
-            is_playing = True
-            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+            video_source = self.hidden_music_queue[0][0]['source']
+            self.is_playing = True
+            self.voice.play(discord.FFmpegPCMAudio(video_source, **self.FFMPEG_OPTIONS), after=lambda e: self.play_previous())
         else:
-            is_playing = False
+            self.is_playing = False
 
     async def play_music(self):
         if len(self.music_queue) > 0:
             self.is_playing = True
-            m_url = self.music_queue[0][0]['source']
+            video_source = self.music_queue[0][0]['source']
 
-            if self.vc == "" or not self.vc.is_connected() or self.vc == None:
-                self.vc = await self.music_queue[0][1].connect()
+            if self.voice == "" or not self.voice.is_connected() or self.voice == None:
+                self.voice = await self.music_queue[0][1].connect()
             else:
-                await self.vc.move_to(self.music_queue[0][1])
+                await self.voice.move_to(self.music_queue[0][1])
             self.music_queue.pop(0)
 
-            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+            self.voice.play(discord.FFmpegPCMAudio(video_source, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
         else:
             self.is_playing = False
 
-    @commands.command(name="help", help="Displays all the available commands")
+    @commands.command(name="help")
     async def help(self, ctx):
         await ctx.send(self.help_message)
 
-    @commands.command(name="play", help="Plays a selected song from youtube")
+    @commands.command(name="hello")
+    async def hello(self, ctx):
+        await ctx.send(self.greeting_message)
+
+    @commands.command(name="play")
     async def play(self, ctx, *args):
         query = " ".join(args)
         
         voice_channel = ctx.author.voice.channel
-        if voice_channel is None:
-            await ctx.send("Kindly connect to a voice channel!")
+        song = self.search_yt(query)
+        if type(song) == type(True):
+            await ctx.send("Error : Could not find/download the song.")
         else:
-            song = self.search_yt(query)
-            if type(song) == type(True):
-                await ctx.send("Could not download the song. Incorrect format try another keyword. This could be due to playlist or a livestream format.")
-            else:
-                await ctx.send("Song will be played!!")
-                self.music_queue.append([song, voice_channel])
+            await ctx.send("Song will be played!!")
+            self.music_queue.append([song, voice_channel])
+            self.hidden_music_queue.append([song, voice_channel])
                 
-                if self.is_playing == False:
-                    await self.play_music()
+            if self.is_playing == False:
+                await self.play_music()
 
-    @commands.command(name="queue", help="Displays the current songs in queue")
+    @commands.command(name="queue")
     async def queue(self, ctx):
-        retval = ""
+        music_list_queue = ""
         for i in range(0, len(self.music_queue)):
-            retval += self.music_queue[i][0]['title'] + "\n"
+            music_list_queue += self.music_queue[i][0]['title'] + "\n"
 
-        print(retval)
-        if retval != "":
-            await ctx.send(retval)
+        print(music_list_queue)
+        if music_list_queue != "":
+            await ctx.send(music_list_queue)
         else:
-            await ctx.send("No music in queue")
+            await ctx.send("Queue is empty!!")
 
     @commands.command()
     async def pause(self, ctx):
-        if self.vc.is_playing():
-            self.vc.pause()
+        if self.voice.is_playing():
+            self.voice.pause()
         else:
-            await ctx.send("Currently no audio is playing.")
+            await ctx.send("Error : Nothing playing!")
 
 
     @commands.command()
     async def resume(self, ctx):
-        if self.vc.is_paused():
-            self.vc.resume()
+        if self.voice.is_paused():
+            self.voice.resume()
         else:
             await ctx.send("The audio is not paused.")
 
 
     @commands.command()
     async def stop(self, ctx):
-        await self.vc.stop()
+        await self.voice.stop()
 
-    @commands.command(name="skip", help="Skips the current song being played")
+    @commands.command(name="skip")
     async def skip(self, ctx):
-        if self.vc != "" and self.vc:
-            self.vc.stop()
+        if self.voice != "" and self.voice:
+            self.voice.stop()
             await self.play_music()
 
-    @commands.command(name="previous", help="Plays previous song!")
+    @commands.command(name="previous")
     async def previous(self, ctx):
-        if self.vc != "" and self.vc:
-            self.vc.stop()
+        if self.voice != "" and self.voice:
+            self.voice.stop()
             await self.play_music()
 
     @commands.command()
     async def disconnect(self, ctx):
         global music_queue
-        if self.vc.is_connected():
-            await self.vc.disconnect()
+        if self.voice.is_connected():
+            await self.voice.disconnect()
             self.music_queue.clear()
         else:
-            await ctx.send("The bot is not connected to a voice channel.")
+            await ctx.send("Error : Bot is already diconnected!!")
